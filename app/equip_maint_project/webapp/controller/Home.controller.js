043c33598dd5f556bd09952594a10b7def1eca13
VENKATA MANIKANTA GUPTHA GADAMSETTI <gupthagvm10@gmail.com>
2024-06-03 12:52:45 +0000

    Service Date From and to were added

--- app/equip_maint_project/webapp/controller/Home.controller.js
+++ app/equip_maint_project/webapp/controller/Home.controller.js
@@ -5,70 +5,73 @@ sap.ui.define([
     'sap/ui/core/format/DateFormat'
 ], function(Controller, Filter, FilterOperator, DateFormat) {
     "use strict";
- 
+
     return Controller.extend("com.sap.equipmaintproject.controller.Home", {
         onInit: function() {
             var oView = this.getView();
             var oTable = oView.byId("table0");
             var oMultiComboBox1 = oView.byId("MCB01");
-            var oMultiComboBox2 = oView.byId("MCB02");
+            
             var oMultiComboBox3 = oView.byId("MCB03");
+            var oDatePickerFrom = oView.byId("DP01_From");
+            var oDatePickerTo = oView.byId("DP01_To");
             var oDateFormat = DateFormat.getDateInstance({
-                pattern: "YYYY-MM-dd" // Set the desired date format
+                pattern: "YYYY-MM-dd"
             });
 
             function doFiltering() {
                 var aFilters = [];
+        
+                // Plant filter
                 var sSelectedPlant = oMultiComboBox1.getSelectedKeys();
-                var sSelectedServiceDate = oMultiComboBox2.getSelectedKeys().map(function(dateString) {
-                    var dateObject = new Date(dateString);
-                    var formattedDate = oDateFormat.format(dateObject);
-                    return formattedDate;
-                });
-                var sSelectedMachineID = oMultiComboBox3.getSelectedKeys();
- 
                 if (sSelectedPlant.length > 0) {
                     var plantFilters = sSelectedPlant.map(function(plant) {
                         return new Filter("plant", FilterOperator.EQ, plant);
                     });
                     aFilters.push(new Filter({
                         filters: plantFilters,
-                        and: false 
+                        and: false
                     }));
                 }
-               
-                if (sSelectedServiceDate.length > 0) {
-                    var serviceDateFilters = sSelectedServiceDate.map(function(serviceDate) {
-                        return new Filter("serviceDate", FilterOperator.EQ, serviceDate);
+        
+                var sDateFrom = oDatePickerFrom.getDateValue();
+                var sDateTo = oDatePickerTo.getDateValue();
+                if (sDateFrom && sDateTo) {
+                    var formattedDateFrom = oDateFormat.format(sDateFrom);
+                    var formattedDateTo = oDateFormat.format(sDateTo);
+                    var dateFilters = new Filter({
+                        filters: [
+                            new Filter("serviceDate", FilterOperator.BT, formattedDateFrom, formattedDateTo)
+                        ],
+                        and: true
                     });
-                    aFilters.push(new Filter({
-                        filters: serviceDateFilters,
-                        and: false 
-                    }));
-                }    
- 
+                    aFilters.push(dateFilters);
+                }
+        
+                var sSelectedMachineID = oMultiComboBox3.getSelectedKeys();
                 if (sSelectedMachineID.length > 0) {
                     var machineFilters = sSelectedMachineID.map(function(machineID) {
                         return new Filter("machineID", FilterOperator.EQ, machineID);
                     });
                     aFilters.push(new Filter({
                         filters: machineFilters,
-                        and: false 
+                        and: false
                     }));
                 }
-                
-                var oBinding = oTable.getBinding("items");                
+        
+                // Apply filters to table binding
+                var oBinding = oTable.getBinding("items");
                 oBinding.filter(new Filter({
                     filters: aFilters,
-                    and: true 
+                    and: true
                 }));
             }
-
-            oView.byId("goButton").attachPress(doFiltering);            
+        
+            oView.byId("goButton").attachPress(doFiltering);
         },
         onInputChange: function(oEvent) {
             try {
-                var sNewValue = oEvent.getParameter("value");        
+                var sNewValue = oEvent.getParameter("value");
                 var sPath = oEvent.getSource().getBindingContext("mainModel").getPath();
                 var oModel = this.getView().getModel("mainModel");
                 sap.m.MessageToast.show("Updated new value: " + sNewValue);
@@ -92,17 +95,17 @@ sap.ui.define([
                 }.bind(this)
             });
         },
-        
+
         onDeleteSelected: function() {
             try {
                 var oTable = this.getView().byId("table0");
                 var aSelectedItems = oTable.getSelectedItems();
-        
+
                 if (aSelectedItems.length === 0) {
                     sap.m.MessageToast.show("Select entries to delete.");
-                    return; 
+                    return;
                 }
-        
+
                 sap.m.MessageBox.confirm("Delete selected entries?", {
                     icon: sap.m.MessageBox.Icon.WARNING,
                     actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
@@ -111,7 +114,7 @@ sap.ui.define([
                             aSelectedItems.forEach(function(oSelectedItem) {
                                 var oContext = oSelectedItem.getBindingContext("mainModel");
                                 var sUserName = oContext.getProperty("maintID");
-                                
+
                                 oContext.delete().then(function() {
                                     sap.m.MessageToast.show("Entries deleted successfully");
                                 }.bind(this), function(oError) {
@@ -122,14 +125,14 @@ sap.ui.define([
                                     sap.m.MessageBox.error(oError.message + ": " + sUserName);
                                 }.bind(this));
                             }.bind(this));
-                            
+
                             oTable.removeSelections();
                         } else {
                             sap.m.MessageToast.show("Deletion cancelled.");
                         }
                     }.bind(this)
                 });
-        
+
             } catch (error) {
                 console.error("Error occurred in onDeleteSelected:", error);
                 sap.m.MessageToast.show("Error deleting entries!", {
@@ -164,11 +167,11 @@ sap.ui.define([
             // Create a new entry through the table's list binding
             var oContext = oBinding.create(oNewEntry);
 
-            this._setBusy(flase);
+            this._setBusy(false);
             this.getView().getModel("appView").setProperty("/usernameEmpty", true);
 
             // Select and focus the table row that contains the newly created entry
-            oList.getItems().some(function (oItem) {
+            oList.getItems().some(function(oItem) {
                 if (oItem.getBindingContext() === oContext) {
                     oItem.focus();
                     oItem.setSelected(true);
@@ -176,7 +179,6 @@ sap.ui.define([
                 }
             });
 
-            // Close dialog
             oDialog.close();
         },
 
@@ -184,7 +186,7 @@ sap.ui.define([
             var oDialog = this.byId("createDialog");
             oDialog.close();
         },
-        
+
         _setBusy: function(bBusy) {
             this.getView().setBusy(bBusy);
         }
